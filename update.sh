482411c6a86b2646d5afc711c20bd996664910c2
Christian Elberfeld <elberfeld@web.de>
2015-11-12 19:16:30 +0100

    update nur noch in minuten-intervall

--- update.sh
+++ update.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-for i in `seq 1 6`;
-do
-  python /var/node-stats/main.py -server 10.43.0.12 -port 2003
-  sleep 10
-done
+python /var/node-stats/main.py -server 10.43.0.12 -port 2003
 
 
