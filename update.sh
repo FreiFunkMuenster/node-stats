97d46750565a49b4eb4794aa1ba4de5ca2f22922
Michael Suelmann <suelmann@gmx.de>
2016-03-23 23:46:57 +0100

    Umstellung auf Graphite auf der Service-VM

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in /proc/sys/net/ipv4/conf/bat*; do
     num=${i#*bat}
-    python /usr/src/node-stats/main.py --server=192.168.43.4 --port=2003 --domain=domaene-$(printf %02d ${num}) --socket=/run/alfred.$(printf %02d ${num}).sock
+    python /usr/src/node-stats/main.py --server=localhost --port=2003 --domain=domaene-${num} --socket=/run/alfred.${num}.sock
 done
