f89767efac7a1c5ce9071cee8bacd88ad9ae199c
Simon Wüllhorst <mail@simon-wuellhorst.de>
2017-03-20 20:16:43 +0100

    Removed update.sh files, because they are part of our Ansible repo now.

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in /proc/sys/net/ipv4/conf/bat*; do
-    num=${i#*bat}
-    python /usr/src/node-stats/main.py --server=148.251.101.196 --port=2003 --domain=domaene-${num} --socket=/run/alfred.${num}.sock
-done
